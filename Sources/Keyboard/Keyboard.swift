f9731737d913e53b0300f24e4c5fb97c643856e3
Taylor Holliday <taylor@audulus.com>
2022-06-01 16:15:05 -0700

    Update Keyboard.swift

--- Sources/Keyboard/Keyboard.swift
+++ Sources/Keyboard/Keyboard.swift
@@ -1,16 +1,27 @@
 import SwiftUI
+import Tonic
 
 public struct Key: View {
 
+    var note: Note
+
     public var body: some View {
         Rectangle()
+            .frame(width: 44, height: 200)
+            .foregroundColor(.red)
     }
 }
 
 public struct Keyboard: View {
 
+    let notes = (0...127).map({ Pitch($0).note(in: .C) })
+
     public var body: some View {
-        Text("Test")
+        HStack {
+            ForEach(notes, id: \.self) { note in
+                Key(note: note)
+            }
+        }
     }
 }
 
