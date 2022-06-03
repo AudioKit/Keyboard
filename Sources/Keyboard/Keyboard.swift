6ac9df6231ada998b8ec16d7bf5b34c66ed13ad8
Aurelius Prochazka <aurephd@gmail.com>
2022-06-02 23:08:38 -0700

    Made the touchedNotes dictionary use Note, made size based on frame given, add colors definitions

--- Sources/Keyboard/Keyboard.swift
+++ Sources/Keyboard/Keyboard.swift
@@ -11,19 +11,19 @@ extension CGPoint: Hashable {
 
 public struct KeyboardKey: View {
 
-    var midiNote: Int
+    var midiNote: Int8
     var note: Note {
-        Pitch(intValue: midiNote).note(in: model.key)
+        Pitch(midiNote).note(in: model.key)
     }
     var keyColor: Color {
-        let baseColor: Color = Pitch(intValue: midiNote).note(in: .C).accidental == .natural ? .white : .black
-        if model.activatedNotes.values.contains(midiNote) {
-            return .red
+        let baseColor: Color = Pitch(midiNote).note(in: .C).accidental == .natural ? .white : .black
+        if (model.highlightedNotes + model.touchedNotes.values).map({ $0.noteNumber }).contains(midiNote) {
+            return model.noteColors(NoteClass(intValue: Int(midiNote) % 12))
         }
         return baseColor
     }
     var textColor: Color {
-        return Pitch(intValue: midiNote).note(in: .C).accidental == .natural ? .black : .white
+        return Pitch(midiNote).note(in: .C).accidental == .natural ? .black : .white
     }
     @ObservedObject var model: KeyboardModel
 
@@ -37,24 +37,24 @@ public struct KeyboardKey: View {
     }
 
     func rect(rect: CGRect) -> some View {
-        print("setting keyRect for \(note) to \(rect)")
         model.keyRects[note] = rect
         return ZStack(alignment: .bottom) {
             Rectangle()
             .foregroundColor(keyColor)
-            if model.shouldDisplayNotes {
+            if model.shouldDisplayNoteNames {
                 Text("\(note.description)")
+                    .font(Font(.init(.system, size: rect.width / 3)))
                     .foregroundColor(textColor)
-                    .padding()
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: rect.size.height / 20, trailing: 0))
             }
         }
         .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ gesture in
-            if let note = findNote(location: gesture.location)?.noteNumber {
-                model.activatedNotes[gesture.startLocation] = Int(note)
+            if let note = findNote(location: gesture.location) {
+                model.touchedNotes[gesture.startLocation] = note
             }
         })
             .onEnded({ gesture in
-                model.activatedNotes.removeValue(forKey: gesture.startLocation)
+                model.touchedNotes.removeValue(forKey: gesture.startLocation)
             })
         )
     }
@@ -63,20 +63,28 @@ public struct KeyboardKey: View {
         GeometryReader { proxy in
             rect(rect: proxy.frame(in: .global))
         }
-        .frame(width: 60, height: 200)
     }
 }
 
 class KeyboardModel: ObservableObject {
-    var midiNotes = (60...84)
-    var shouldDisplayNotes: Bool = false
+    var midiNotes: ClosedRange<Int8> = (60...72)
+    var shouldDisplayNoteNames: Bool = false
     var key: Key = .C
-    @Published var activatedNotes: [CGPoint:Int] = [:]
+    @Published var touchedNotes: [CGPoint:Note] = [:]
+    @Published var highlightedNotes: [Note] = []
+    var noteColors: (NoteClass)->Color = { noteClass in
+        return [.red, .orange, .yellow, .mint, .green, .teal, .cyan, .blue, .indigo, .purple, .pink, .init(red: 1.0, green: 0.33, blue: 0.33)][noteClass.intValue]
+    }
 
     init(key: Key = .C,
-         shouldDisplayNotes: Bool = false) {
+         shouldDisplayNoteNames: Bool = false,
+         noteColors: ((NoteClass)->Color)? = nil
+    ) {
         self.key = key
-        self.shouldDisplayNotes = shouldDisplayNotes
+        self.shouldDisplayNoteNames = shouldDisplayNoteNames
+        if let colors = noteColors {
+            self.noteColors = colors
+        }
     }
 
     // Computed key rectangles
@@ -87,7 +95,7 @@ class KeyboardModel: ObservableObject {
 public struct Keyboard: View {
 
     @StateObject var model = KeyboardModel(key: .F,
-                                           shouldDisplayNotes: true)
+                                           shouldDisplayNoteNames: true)
 
     public init() { }
 
@@ -97,6 +105,8 @@ public struct Keyboard: View {
                 KeyboardKey(midiNote: note, model: model)
             }
         }
+        .frame(minWidth: 600, minHeight: 100)
+
     }
 }
 
