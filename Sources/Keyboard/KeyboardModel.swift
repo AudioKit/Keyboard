e2b7b35614b546522902d101dc2298fb392a8f79
Taylor Holliday <taylor@audulus.com>
2022-06-04 17:48:37 -0700

    Update KeyboardModel.swift

--- Sources/Keyboard/KeyboardModel.swift
+++ Sources/Keyboard/KeyboardModel.swift
@@ -6,7 +6,13 @@ class KeyboardModel: ObservableObject {
     @Published var touchedPitches: [CGPoint: Pitch] = [:]
     @Published var highlightedPitches: [Pitch] = []
 
-    // Computed key rectangles
+    /// Computed key rectangles.
+    /// This is not @Published because we populate it as a side-effect
+    /// of our body function. If it were @Published, that would cause
+    /// runtime errors.
+    ///
+    /// This also forces us to use an ObservableObject instead of a Binding
+    /// for the keyboard state.
     var keyRects: [Pitch: CGRect] = [:]
 
 }
