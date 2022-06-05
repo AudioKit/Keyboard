c8084b16edd602cafed41d030cc763db3c06bb62
Aurelius Prochazka <aurephd@gmail.com>
2022-06-04 20:41:32 -0700

    Updated demo contentview

--- Demo/Shared/ContentView.swift
+++ Demo/Shared/ContentView.swift
@@ -14,14 +14,18 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Keyboard2(noteOn: noteOn, noteOff: noteOff) { pitch, state in
+            PianoKeyboard(noteOn: noteOn, noteOff: noteOff) { pitch, state in
                 KeyboardKey(pitch: pitch, model: state)
             }
-            Keyboard2(pitchRange: Pitch(48)...Pitch(65)) { pitch, state in
+            IsomorphicKeyboard(pitchRange: Pitch(48)...Pitch(65)) { pitch, state in
                 KeyboardKey(pitch: pitch, model: state)
             }
-            Keyboard2(latching: true, noteOn: noteOn, noteOff: noteOff) { pitch, state in
-                KeyboardKey(pitch: pitch, model: state)
+            PianoKeyboard(latching: true, noteOn: noteOn, noteOff: noteOff) { pitch, state in
+                if state.touchedPitches.values.contains(pitch) {
+                    Rectangle().foregroundColor(.black).overlay(Text("hi"))
+                } else {
+                    Rectangle().foregroundColor(Color(red: Double.random(in: 0...1), green:     Double.random(in: 0...1), blue: Double.random(in: 0...1), opacity: 1))
+                }
             }
         }
     }
