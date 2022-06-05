7915873b30e112fb896d29949d5e6a6136457e35
Aurelius Prochazka <aurephd@gmail.com>
2022-06-05 11:52:11 -0700

    Removed out of date code from playground

--- Keyboard.playground/Contents.swift
+++ Keyboard.playground/Contents.swift
@@ -6,15 +6,6 @@ import PlaygroundSupport
 struct ContentView: View {
     var body: some View {
         Keyboard()
-        Keyboard(settings: KeyboardSettings(externalPitchSet: PitchSet()))
-        Keyboard(settings: KeyboardSettings(pitchRange: Pitch(48)...Pitch(65)))
-        Keyboard(settings: KeyboardSettings(key: .F))
-        Keyboard(settings: KeyboardSettings(shouldDisplayNoteNames: false))
-        Keyboard(settings: KeyboardSettings(noteColors: { noteClass in
-            [.red, .orange, .yellow, .mint, .green,
-                .teal, .cyan, .blue, .indigo, .purple, .pink,
-                .init(red: 1.0, green: 0.33, blue: 0.33)][noteClass.intValue % 12]
-        }))
     }
 }
 
