3dcd1b291a108f1bdc1a805d8163006f5346ad57
Aurelius Prochazka <aurephd@gmail.com>
2022-08-04 13:17:37 -0700

    Corrected dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Keyboard",
     platforms: [ .macOS(.v12), .iOS(.v15)],
     products: [.library(name: "Keyboard", targets: ["Keyboard"])],
-    dependencies: [.package(url: "https://github.com/Audulus/Tonic.git", branch: "main")],
+    dependencies: [.package(url: "https://github.com/AudioKit/Tonic.git", from: "1.0.0")],
     targets: [
         .target(name: "Keyboard", dependencies: ["Tonic"]),
         .testTarget(name: "KeyboardTests", dependencies: ["Keyboard"]),
