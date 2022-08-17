9168ee45ed3a0d3f81399e97763d635da13991ad
Aurelius Prochazka <aurephd@gmail.com>
2022-08-17 11:32:35 -0700

    Display only correct notes in key on isomorphic keyboard

--- Sources/Keyboard/Layouts/Isomorphic.swift
+++ Sources/Keyboard/Layouts/Isomorphic.swift
@@ -8,9 +8,22 @@ struct Isomorphic<Content>: View where Content: View {
     var root: NoteClass
     var scale: Scale
 
+    var pitchesToShow: [Pitch] {
+        var pitchArray: [Pitch] = []
+        let key = Key(root: root, scale: scale)
+        for pitch in pitchRange {
+            // TODO this math should make it into Tonic as something like:
+            // pitch(in: key, withoutAccidental: true))
+            if key.noteSet.array.map({ $0.noteClass }).contains(pitch.note(in: key).noteClass) {
+                pitchArray.append(pitch)
+            }
+        }
+        return Array(pitchArray)
+    }
+
     var body: some View {
         HStack(spacing: 0) {
-            ForEach(pitchRange, id: \.self) { pitch in
+            ForEach(pitchesToShow, id: \.self) { pitch in
                 KeyContainer(model: model,
                              pitch: pitch,
                              content: content)
