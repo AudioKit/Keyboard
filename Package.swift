14e53ba0272ceb1bc765bf6ea29f0e2af0b96f7e
Aurelius Prochazka <aurephd@gmail.com>
2022-08-17 13:14:21 -0700

    Update package dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Keyboard",
     platforms: [.macOS(.v12), .iOS(.v15)],
     products: [.library(name: "Keyboard", targets: ["Keyboard"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/Tonic.git", from: "1.0.0")],
+    dependencies: [.package(url: "https://github.com/AudioKit/Tonic.git", from: "1.0.6")],
     targets: [
         .target(name: "Keyboard", dependencies: ["Tonic"]),
         .testTarget(name: "KeyboardTests", dependencies: ["Keyboard"]),
